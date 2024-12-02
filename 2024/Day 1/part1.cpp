b868a30e0d08d72620345fed453fcaac748a1837
Sharpless298 <sharpless298@gmail.com>
2024-12-03 04:10:44 +0800

    first commit

---
+++ 2024/Day 1/part1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+signed main() {
+	freopen("input.txt", "r", stdin);
+	
+	int x, y;
+	vector<int> a, b;
+	while(cin >> x >> y) 
+		a.push_back(x), b.push_back(y);
+	
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+
+	int sum = 0;
+	for(int i=0; i<(int)a.size(); i++) 
+		sum += abs(a[i]-b[i]);
+	
+	cout << sum << '\n';
+}
---
+++ 2024/Day 2/part1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+signed main() {
+	freopen("input.txt", "r", stdin);
+
+	int ans = 0;
+	string s;
+	while(getline(cin, s)) {
+		stringstream ss(s);
+
+		vector<int> a;
+		for(int x; ss>>x; a.push_back(x));
+		
+		bool ok = true;
+		for(int i=0; i<(int)a.size()-1; i++) {
+			int d = a[i]-a[i+1];
+			if(d>=0 || d<-3) {
+				ok = false;
+				break;
+			}
+		}
+		if(ok) {ans++; continue;}
+
+		ok = true;
+		for(int i=0; i<(int)a.size()-1; i++) {
+			int d = a[i]-a[i+1];
+			if(d<=0 || d>3) {
+				ok = false;
+				break;
+			}
+		}
+		if(ok) ans++;
+	}
+
+	cout << ans << '\n';
+}
+
+
