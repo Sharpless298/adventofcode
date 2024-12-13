adbbd9ea6084e2891c3ba63d69fbc8c1533e0b54
Sharpless298 <sharpless298@gmail.com>
2024-12-13 10:36:05 +0800

    upd

---
+++ 2024/Day 11/part1-1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+signed main() {
+	freopen("input.txt", "r", stdin);
+
+	long long x;
+	vector<long long> a;
+	while(cin >> x) a.push_back(x);
+	
+	auto f = [&](long long t) {
+		int cnt = 0;
+		while(t) t /= 10, cnt++;
+		return cnt;
+	};
+	for(int i=0; i<25; i++) {
+		for(int j=(int)a.size()-1; j>=0; j--) {
+			int d = f(a[j]);
+
+			if(a[j] == 0) a[j] = 1;
+			else if(d%2 == 0) {
+				long long t = 1;
+				for(int k=0; k<d/2; k++)
+					t *= 10;
+				a.push_back(a[j]/t);
+				a[j] %= t;
+			}
+			else a[j] *= 2024;
+		}
+	}
+	
+	cout << a.size() << '\n';
+}
---
+++ 2024/Day 11/part1-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+long long ans = 0;
+map<pair<long long, int>, long long> mp;
+
+long long f(long long x, int b) {
+	if(b == 25) return 1LL;
+
+	if(x == 0) {
+		if(!mp[make_pair(0, b)]) return mp[make_pair(0, b)] = f(1, b+1);
+		return mp[make_pair(0, b)];
+	}
+
+	string s = to_string(x);
+	if(s.size()%2 == 0) {
+		long long l = stoll(s.substr(0, s.size()/2)), r = stoll(s.substr(s.size()/2));
+		if(!mp[make_pair(x, b)]) mp[make_pair(x, b)] = f(l, b+1) + f(r, b+1);
+		return mp[make_pair(x, b)];
+	}
+
+	if(!mp[make_pair(x, b)]) mp[make_pair(x, b)] = f(2024*x, b+1);
+	return mp[make_pair(x, b)];
+}
+
+signed main() {
+	freopen("input.txt", "r", stdin);
+	
+	long long x;
+	vector<long long> a;
+	while(cin >> x) a.push_back(x);
+	
+	for(long long i : a) 
+		ans += f(i, 0);
+
+	cout << ans << '\n';
+}
